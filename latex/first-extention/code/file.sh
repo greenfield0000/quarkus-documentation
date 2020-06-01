9a0b780e728af3a85ffc317de920d0801634f1c0
roman <greenfield0000@gmail.com>
2020-06-01 15:57:43 +0300

    add documentation with write extention

---
+++ latex/first-extention/code/file.sh
@@ -0,0 +1,5 @@
+mvn io.quarkus:quarkus-maven-plugin:1.4.2.Final:create-extension -N \
+    -DgroupId=org.example \ 
+    -DartifactId=quarkus-notification \  
+    -Dversion=1.0-SNAPSHOT \ 
+    -Dquarkus.nameBase="Mail sender with up context"
